568a9b0a10d25f21700397875455c4aa46e9baf7
jbdwwm48 <jb.charbonnier.dwwm48@gmail.com>
2024-12-11 10:07:51 +0100

    update HTML&CSS .1

---
+++ index.js
@@ -0,0 +1,10 @@
+//darkmode
+    function lightMode() {
+        document.body.classList.replace('darkMode','lightMode')
+        console.log("TA GUEULE!!!!");
+        
+    }
+    function darkMode() {
+        document.body.classList.replace('lightMode','darkMode')
+        console.log("TA GUEULE!!!!");
+    }    
